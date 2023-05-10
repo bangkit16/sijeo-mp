11c16c15711400b81ce68a5f45addda8211573fb
rasyidrazeka <rasyidrazeka@gmail.com>
2023-05-10 17:03:34 +0700

    bangkit mau bantu

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,23 +7,22 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <style>
-
     </style>
 </head>
 
 <body>
     <div class="">
-        <div class="login text-center">
+        <div class="login" style="overflow: hidden;">
             <div class="row">
                 <div class="col-7">
-                    <img src="<?= base_url(); ?>/img/login image.png" class="img-fluid" alt="">
+                    <img src="<?= base_url(); ?>/img/login image.png" class="img-fluid" alt="" style="height: 20cm; width: 100%";>
                 </div>
-                <div class="col-5">
+                <div class="col-5 mt-5">
                     <div class="row">
-                        <div class="col" style="text-align: left;">
+                        <div class="col" style="text-align: left; margin-left: 40px;">
                             <h2>Masuk</h2>
                         </div>
-                        <div class="col" style="text-align: right;">
+                        <div class="col" style="text-align: right; margin-right: 60px; color: #FFA931;">
                             <h2>Daftar</h2>
                         </div>
                     </div>
