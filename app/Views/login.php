1233813aad795a1eaa8cf44594e66dd4e726bdec
bangkit16 <bangkitcaniago@gmail.com>
2023-05-10 17:05:13 +0700

    sd

--- app/Views/login.php
+++ app/Views/login.php
@@ -6,27 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <style>
-
+        * {
+            font-family: 'Poppins', sans-serif;
+        }
     </style>
 </head>
 
 <body>
     <div class="">
-        <div class="login text-center">
+        <div class="login">
             <div class="row">
                 <div class="col-7">
                     <img src="<?= base_url(); ?>/img/login image.png" class="img-fluid" alt="">
                 </div>
                 <div class="col-5">
-                    <div class="row">
-                        <div class="col" style="text-align: left;">
-                            <h2>Masuk</h2>
+                    <!-- <div class="container"> -->
+                        <div class="row mt-5">
+                            <div class="col" style="">
+                                <h2>Masuk</h2>
+                            </div>
+                            <div class="col" style="">
+                                <h2>Daftar</h2>
+                            </div>
                         </div>
-                        <div class="col" style="text-align: right;">
-                            <h2>Daftar</h2>
-                        </div>
-                    </div>
+                    <!-- </div> -->
                 </div>
             </div>
         </div>
