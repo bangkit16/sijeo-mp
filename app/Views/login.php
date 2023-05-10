c5173c346390cef8d8f652db6ea5ee836470268b
rasyidrazeka <rasyidrazeka@gmail.com>
2023-05-10 17:20:44 +0700

    bangkit mau coomit

--- app/Views/login.php
+++ app/Views/login.php
@@ -6,7 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
     <style>
+        * {
+            font-family: 'Poppins', sans-serif;
+        }
     </style>
 </head>
 
@@ -15,15 +21,15 @@
         <div class="login" style="overflow: hidden;">
             <div class="row">
                 <div class="col-7">
-                    <img src="<?= base_url(); ?>/img/login image.png" class="img-fluid" alt="" style="height: 20cm; width: 100%";>
+                    <img src="<?= base_url(); ?>/img/login image.png" class="img-fluid" alt="" style="height: 20cm; width: 100%; ";>
                 </div>
                 <div class="col-5 mt-5">
                     <div class="row">
                         <div class="col" style="text-align: left; margin-left: 40px;">
-                            <h2>Masuk</h2>
+                            <h2 style="font-size: 40px;">Masuk</h2>
                         </div>
                         <div class="col" style="text-align: right; margin-right: 60px; color: #FFA931;">
-                            <h2>Daftar</h2>
+                            <h2 style="font-size: 32px;">Daftar</h2>
                         </div>
                     </div>
                 </div>
