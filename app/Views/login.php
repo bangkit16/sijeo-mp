4a4f82c11fa378a4582768c0b3614467a6743ef6
rasyidrazeka <rasyidrazeka@gmail.com>
2023-05-10 15:10:05 +0700

    aku bangkit

--- app/Views/login.php
+++ app/Views/login.php
@@ -6,11 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <style>
+
+    </style>
 </head>
 
 <body>
-    <div class="container-fluid">
-        
+    <div class="">
+        <div class="login text-center">
+            <div class="row">
+                <div class="col-7">
+                    <img src="<?= base_url(); ?>/img/login image.png" class="img-fluid" alt="">
+                </div>
+                <div class="col-5">
+                    <div class="row">
+                        <div class="col" style="text-align: left;">
+                            <h2>Masuk</h2>
+                        </div>
+                        <div class="col" style="text-align: right;">
+                            <h2>Daftar</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
