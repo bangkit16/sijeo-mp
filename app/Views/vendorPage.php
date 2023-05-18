f4de573ecfafada5e24420d1788dda6626de6781
rasyidrazeka <rasyidrazeka@gmail.com>
2023-05-18 16:49:06 +0700

    baru

--- app/Views/vendorPage.php
+++ app/Views/vendorPage.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -15,21 +15,51 @@
             padding: 0;
             font-family: 'Poppins', sans-serif;
         }
-        .garis_vertikal{
+
+        .garis_vertikal {
             border-left: 1px black solid;
             height: 25px;
             width: 0px;
         }
-        .bg-1-lanpaggue{
+
+        .bg-1-lanpaggue {
             background-image: url("<?= base_url(); ?>/img/landing page 1.png");
             background-size: cover;
             background-repeat: no-repeat;
         }
-        #link-nav2:hover{
+
+        #link-nav2:hover {
             color: #FFA931 !important;
+        }
+
+        .gallery {
+
+            padding: 1rem;
+            display: grid;
+            grid-template-columns: repeat(10, 80vw);
+            grid-template-rows: 1fr;
+            grid-column-gap: 5rem;
+            grid-row-gap: 5rem;
+            overflow: hidden;
+            height: 90vh;
+            scroll-snap-type: both mandatory;
+            scroll-padding: 1rem;
+            transition: 1sec;
+        }
+
+        .active {
+            scroll-snap-type: unset;
+        }
+
+        li {
+            scroll-snap-align: center;
+            display: inline-block;
+            border-radius: 3px;
+            font-size: 0;
         }
     </style>
 </head>
+
 <body>
     <nav>
         <div class="container-fluid text-center sticky-top" style="background-color: white;">
@@ -42,38 +72,86 @@
 
                 </div>
                 <div class="mx-3" style="font-size: 14px;">
-                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 25px; height: 25px; fill: black;"><title>bell-ring-outline</title><path d="M10,21H14A2,2 0 0,1 12,23A2,2 0 0,1 10,21M21,19V20H3V19L5,17V11C5,7.9 7.03,5.17 10,4.29C10,4.19 10,4.1 10,4A2,2 0 0,1 12,2A2,2 0 0,1 14,4C14,4.1 14,4.19 14,4.29C16.97,5.17 19,7.9 19,11V17L21,19M17,11A5,5 0 0,0 12,6A5,5 0 0,0 7,11V18H17V11M19.75,3.19L18.33,4.61C20.04,6.3 21,8.6 21,11H23C23,8.07 21.84,5.25 19.75,3.19M1,11H3C3,8.6 3.96,6.3 5.67,4.61L4.25,3.19C2.16,5.25 1,8.07 1,11Z" /></svg></a>
+                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 25px; height: 25px; fill: black;">
+                            <title>bell-ring-outline</title>
+                            <path d="M10,21H14A2,2 0 0,1 12,23A2,2 0 0,1 10,21M21,19V20H3V19L5,17V11C5,7.9 7.03,5.17 10,4.29C10,4.19 10,4.1 10,4A2,2 0 0,1 12,2A2,2 0 0,1 14,4C14,4.1 14,4.19 14,4.29C16.97,5.17 19,7.9 19,11V17L21,19M17,11A5,5 0 0,0 12,6A5,5 0 0,0 7,11V18H17V11M19.75,3.19L18.33,4.61C20.04,6.3 21,8.6 21,11H23C23,8.07 21.84,5.25 19.75,3.19M1,11H3C3,8.6 3.96,6.3 5.67,4.61L4.25,3.19C2.16,5.25 1,8.07 1,11Z" />
+                        </svg></a>
                 </div>
                 <div class="mx-3" style="font-size: 14px;">
-                    <a href="http://"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 25px; height: 25px; fill: black;"><title>message-processing-outline</title><path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2M20 16H5.2L4 17.2V4H20V16M17 11H15V9H17M13 11H11V9H13M9 11H7V9H9" /></svg></a>
-                </div>
-            </div>
-        </div>
-        <div class="container-fluid text-center bg-1-lanpaggue sticky-top">
-            <div class="d-flex flex-row align-items-center justify-content-end p-2">
-                <div class="me-auto">
-                    <a href="http://localhost:8080/"><img src="<?= base_url(); ?>/img/logo si jeo.png" class="img-fluid" style="width: auto; height: 60px"></a>
-                </div>
-                <div class="mx-5">
-                    <a href="http://localhost:8080/" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Home</a>
-                </div>
-                <div class="mx-5">
-                    <a href="" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Tentang</a>
-                </div>
-                <div class="mx-5">
-                    <a href="http://localhost:8080/vendor" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Vendor</a>
-                </div>
-                <div class="mx-5">
-                    <a href="" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Store</a>
-                </div>
-                <div class="mx-5">
-                    <a href="" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Lastest Event</a>
-                </div>
-                <div class="mx-5">
-                    <a href="" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Review</a>
+                    <a href="http://"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 25px; height: 25px; fill: black;">
+                            <title>message-processing-outline</title>
+                            <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2M20 16H5.2L4 17.2V4H20V16M17 11H15V9H17M13 11H11V9H13M9 11H7V9H9" />
+                        </svg></a>
                 </div>
             </div>
         </div>
     </nav>
+    <div class="container-fluid text-center bg-1-lanpaggue sticky-top">
+        <div class="d-flex flex-row align-items-center justify-content-end p-2">
+
+            <div class="me-auto">
+                <a href="http://localhost:8080/"><img src="<?= base_url(); ?>/img/logo si jeo.png" class="img-fluid" style="width: auto; height: 60px"></a>
+            </div>
+            <div class="mx-5">
+                <a href="<?= base_url(); ?>" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Home</a>
+            </div>
+            <div class="mx-5">
+                <a href="" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Tentang</a>
+            </div>
+            <div class="mx-5">
+                <a href="http://localhost:8080/vendor" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Vendor</a>
+            </div>
+            <div class="mx-5">
+                <a href="" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Store</a>
+            </div>
+            <div class="mx-5">
+                <a href="" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Lastest Event</a>
+            </div>
+            <div class="mx-5">
+                <a href="" id="link-nav2" style="color: white; font-size: 20px; font-weight: bold; text-decoration: none;">Review</a>
+            </div>
+        </div>
+    </div>
+    <!-- <div class="d-flex align-items-center justify-content-center p-2"> -->
+    <div class="mx-auto ">
+        <ul class="gallery ">
+            <li class="active" style="background: #f6bd60;"></li>
+            <li style="background: #f7ede2;"></li>
+            <li style="background: #f5cac3;"></li>
+            <li style="background: #84a59d;"></li>
+            <li style="background: #f28482;"></li>
+        </ul>
+    </div>
+    <!-- </div> -->
+    <script>
+        const slider = document.querySelector('.gallery');
+        let isDown = false;
+        let startX;
+        let scrollLeft;
+
+        slider.addEventListener('mousedown', e => {
+            isDown = true;
+            slider.classList.add('active');
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+        });
+        slider.addEventListener('mouseleave', _ => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+        slider.addEventListener('mouseup', _ => {
+            isDown = false;
+            slider.classList.remove('active');
+        });
+        slider.addEventListener('mousemove', e => {
+            if (!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const SCROLL_SPEED = 3;
+            const walk = (x - startX) * SCROLL_SPEED;
+            slider.scrollLeft = scrollLeft - walk;
+        });
+    </script>
 </body>
+
 </html>
