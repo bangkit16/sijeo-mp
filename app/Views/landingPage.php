6f76e7ef1fe89514a1aeb7b3a9adc13522a91ae8
bangkit16 <bangkitcaniago@gmail.com>
2023-05-18 15:42:14 +0700

    rillll

--- app/Views/landingPage.php
+++ app/Views/landingPage.php
@@ -15,6 +15,7 @@
             padding: 0;
             font-family: 'Poppins', sans-serif;
         }
+
         .btn-primary-custom {
             background-color: #FFA931;
             color: white;
@@ -22,54 +23,65 @@
             padding: 3px 28px;
             align-content: en;
         }
-        .btn-outline-primary-custom{
+
+        .btn-outline-primary-custom {
             border-color: #FFA931;
             color: #FFA931;
             font-size: 14px;
             padding: 3px 28px;
         }
-        .bg-1-lanpaggue{
+
+        .bg-1-lanpaggue {
             background-image: url("<?= base_url(); ?>/img/landing page 1.png");
             background-size: cover;
             background-repeat: no-repeat;
             height: 700px;
         }
-        .btn-siapkan-event{
+
+        .btn-siapkan-event {
             background-color: #FFA931;
             color: white;
             font-size: 25px;
             padding: 4px 30px;
             border-radius: 25px;
         }
-        .btn:hover{
+
+        .btn:hover {
             border-color: white;
             color: white;
         }
-        .btn:active{
+
+        .btn:active {
             border-color: white !important;
             color: white !important;
         }
-        #btn-daftar:hover{
+
+        #btn-daftar:hover {
             background-color: white;
             color: #FFA931;
             border-color: #FFA931;
         }
-        #btn-daftar:active{
+
+        #btn-daftar:active {
             background-color: white !important;
             color: #FFA931 !important;
             border-color: #FFA931 !important;
         }
-        #btn-masuk:hover{
+
+        #btn-masuk:hover {
             background-color: #FFA931;
             color: white;
         }
-        #link-daftar:hover{
+
+        #link-daftar:hover {
             color: black !important;
         }
-        #link-nav2:hover{
+
+        #link-nav2:hover {
             color: #FFA931 !important;
         }
-        #ikon-wa:hover{
+
+        #ikon-wa:hover {
             fill: white;
         }
     </style>
@@ -84,6 +96,9 @@
                 </div>
                 <div class="mx-3" style="font-size: 14px;">
                     <span>Ingin memesan?</span>
+                    <?php if (isset($_SESSION['username'])) : ?>
+                        <?= $_SESSION['username']; ?>
+                    <?php endif; ?>
                 </div>
                 <div class="mx-3" style="font-size: 14px;">
                     <a class="btn btn-outline-primary-custom" href="http://localhost:8080/login" id="btn-masuk">Masuk</a>
@@ -130,7 +145,10 @@
                 </div>
                 <br>
                 <div class="d-flex flex-row">
-                    <a class="btn btn-siapkan-event" href=""> <svg xmlns="http://www.w3.org/2000/svg" id="ikon-wa" viewBox="0 0 25 25" style="width: 30px; height: 30px; fill: white;"><title>whatsapp</title><path d="M12.04 2C6.58 2 2.13 6.45 2.13 11.91C2.13 13.66 2.59 15.36 3.45 16.86L2.05 22L7.3 20.62C8.75 21.41 10.38 21.83 12.04 21.83C17.5 21.83 21.95 17.38 21.95 11.92C21.95 9.27 20.92 6.78 19.05 4.91C17.18 3.03 14.69 2 12.04 2M12.05 3.67C14.25 3.67 16.31 4.53 17.87 6.09C19.42 7.65 20.28 9.72 20.28 11.92C20.28 16.46 16.58 20.15 12.04 20.15C10.56 20.15 9.11 19.76 7.85 19L7.55 18.83L4.43 19.65L5.26 16.61L5.06 16.29C4.24 15 3.8 13.47 3.8 11.91C3.81 7.37 7.5 3.67 12.05 3.67M8.53 7.33C8.37 7.33 8.1 7.39 7.87 7.64C7.65 7.89 7 8.5 7 9.71C7 10.93 7.89 12.1 8 12.27C8.14 12.44 9.76 14.94 12.25 16C12.84 16.27 13.3 16.42 13.66 16.53C14.25 16.72 14.79 16.69 15.22 16.63C15.7 16.56 16.68 16.03 16.89 15.45C17.1 14.87 17.1 14.38 17.04 14.27C16.97 14.17 16.81 14.11 16.56 14C16.31 13.86 15.09 13.26 14.87 13.18C14.64 13.1 14.5 13.06 14.31 13.3C14.15 13.55 13.67 14.11 13.53 14.27C13.38 14.44 13.24 14.46 13 14.34C12.74 14.21 11.94 13.95 11 13.11C10.26 12.45 9.77 11.64 9.62 11.39C9.5 11.15 9.61 11 9.73 10.89C9.84 10.78 10 10.6 10.1 10.45C10.23 10.31 10.27 10.2 10.35 10.04C10.43 9.87 10.39 9.73 10.33 9.61C10.27 9.5 9.77 8.26 9.56 7.77C9.36 7.29 9.16 7.35 9 7.34C8.86 7.34 8.7 7.33 8.53 7.33Z" /></svg> Siapkan Event Bersama Kami</a>
+                    <a class="btn btn-siapkan-event" href=""> <svg xmlns="http://www.w3.org/2000/svg" id="ikon-wa" viewBox="0 0 25 25" style="width: 30px; height: 30px; fill: white;">
+                            <title>whatsapp</title>
+                            <path d="M12.04 2C6.58 2 2.13 6.45 2.13 11.91C2.13 13.66 2.59 15.36 3.45 16.86L2.05 22L7.3 20.62C8.75 21.41 10.38 21.83 12.04 21.83C17.5 21.83 21.95 17.38 21.95 11.92C21.95 9.27 20.92 6.78 19.05 4.91C17.18 3.03 14.69 2 12.04 2M12.05 3.67C14.25 3.67 16.31 4.53 17.87 6.09C19.42 7.65 20.28 9.72 20.28 11.92C20.28 16.46 16.58 20.15 12.04 20.15C10.56 20.15 9.11 19.76 7.85 19L7.55 18.83L4.43 19.65L5.26 16.61L5.06 16.29C4.24 15 3.8 13.47 3.8 11.91C3.81 7.37 7.5 3.67 12.05 3.67M8.53 7.33C8.37 7.33 8.1 7.39 7.87 7.64C7.65 7.89 7 8.5 7 9.71C7 10.93 7.89 12.1 8 12.27C8.14 12.44 9.76 14.94 12.25 16C12.84 16.27 13.3 16.42 13.66 16.53C14.25 16.72 14.79 16.69 15.22 16.63C15.7 16.56 16.68 16.03 16.89 15.45C17.1 14.87 17.1 14.38 17.04 14.27C16.97 14.17 16.81 14.11 16.56 14C16.31 13.86 15.09 13.26 14.87 13.18C14.64 13.1 14.5 13.06 14.31 13.3C14.15 13.55 13.67 14.11 13.53 14.27C13.38 14.44 13.24 14.46 13 14.34C12.74 14.21 11.94 13.95 11 13.11C10.26 12.45 9.77 11.64 9.62 11.39C9.5 11.15 9.61 11 9.73 10.89C9.84 10.78 10 10.6 10.1 10.45C10.23 10.31 10.27 10.2 10.35 10.04C10.43 9.87 10.39 9.73 10.33 9.61C10.27 9.5 9.77 8.26 9.56 7.77C9.36 7.29 9.16 7.35 9 7.34C8.86 7.34 8.7 7.33 8.53 7.33Z" />
+                        </svg> Siapkan Event Bersama Kami</a>
                 </div>
             </div>
         </div>
