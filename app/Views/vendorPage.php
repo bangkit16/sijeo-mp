ce5f2f9442f2968b9a36634d1684dbedbc5a706b
rasyidrazeka <rasyidrazeka@gmail.com>
2023-05-19 07:39:00 +0700

    baru

--- app/Views/vendorPage.php
+++ app/Views/vendorPage.php
@@ -120,27 +120,6 @@
     <nav>
         <div class="container-fluid text-center sticky-top" style="background-color: white;">
             <div class=" d-flex flex-row align-items-center justify-content-end p-2">
-                <div class="mx-3" style="font-size: 14px;">
-                    <img src="<?= base_url(); ?>/img/vector rasyid.png" class="img-fluid" style="width: 30px; height: 30px; border-radius: 100%;">
-                    <span>&nbsp;&nbsp;&nbsp;Rasyid Razeka</span>
-                </div>
-                <div class="mx-3 garis_vertikal" style="font-size: 14px;">
-
-                </div>
-                <div class="mx-3" style="font-size: 14px;">
-                    <a href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 25px; height: 25px; fill: black;">
-                            <title>bell-ring-outline</title>
-                            <path d="M10,21H14A2,2 0 0,1 12,23A2,2 0 0,1 10,21M21,19V20H3V19L5,17V11C5,7.9 7.03,5.17 10,4.29C10,4.19 10,4.1 10,4A2,2 0 0,1 12,2A2,2 0 0,1 14,4C14,4.1 14,4.19 14,4.29C16.97,5.17 19,7.9 19,11V17L21,19M17,11A5,5 0 0,0 12,6A5,5 0 0,0 7,11V18H17V11M19.75,3.19L18.33,4.61C20.04,6.3 21,8.6 21,11H23C23,8.07 21.84,5.25 19.75,3.19M1,11H3C3,8.6 3.96,6.3 5.67,4.61L4.25,3.19C2.16,5.25 1,8.07 1,11Z" />
-                        </svg></a>
-                </div>
-                <div class="mx-3" style="font-size: 14px;">
-                    <a href="http://"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 25px; height: 25px; fill: black;">
-                            <title>message-processing-outline</title>
-                            <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2M20 16H5.2L4 17.2V4H20V16M17 11H15V9H17M13 11H11V9H13M9 11H7V9H9" />
-                        </svg></a>
-                </div>
-            </div> -->
-            <div class=" d-flex flex-row align-items-center justify-content-end p-2">
                 <?php if (isset($_SESSION['username'])) : ?>
                     <div class="mx-3" style="font-size: 14px;">
                         <img src="<?= base_url(); ?>/img/vector rasyid.png" class="img-fluid" style="width: 30px; height: 30px; border-radius: 100%;">
