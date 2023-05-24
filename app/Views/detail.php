905c99511123aece57feedf0388632bca2a75bf6
rasyidrazeka <rasyidrazeka@gmail.com>
2023-05-24 07:52:27 +0700

    baru

--- app/Views/detail.php
+++ app/Views/detail.php
@@ -301,7 +301,7 @@
             <div class="row row-cols-1 row-cols-md-5 g-4">
                 <div class="col">
                     <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="<?= base_url(); ?>/img/vector rasyid.png" class="img-fluid">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
