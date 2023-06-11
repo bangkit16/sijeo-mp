895ca61bff7f91ceeeec3332636a1f630da8afa1
rasyidrazeka <rasyidrazeka@gmail.com>
2023-06-11 13:50:53 +0700

    tayo

--- app/Views/detail.php
+++ app/Views/detail.php
@@ -2,6 +2,11 @@
 <html lang="en">
 
 <head>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -73,6 +78,12 @@
             background-color: white !important;
             color: #FFA931 !important;
             box-shadow: 0 12px 16px 0 rgba(0, 0, 0, 0.24), 0 17px 50px 0 rgba(0, 0, 0, 0.19) !important;
+        }
+
+        .btnUkuran:hover {
+            border: 1px solid #FFA931;
+            background-color: white !important;
+            color: #FFA931 !important;
         }
 
         .btnWarna {
@@ -164,6 +175,21 @@
         .custom-form {
             border-bottom: 1px solid black !important;
             border-radius: 0%;
+        }
+
+        .inline-group {
+            max-width: 9rem;
+            padding: .5rem;
+        }
+
+        .inline-group .form-control {
+            text-align: right;
+        }
+
+        .form-control[type="number"]::-webkit-inner-spin-button,
+        .form-control[type="number"]::-webkit-outer-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
         }
     </style>
 </head>
@@ -404,103 +430,155 @@
                 </div>
                 <div class="col-md-8">
                     <div class="card-body pt-0">
-                        <h5 class="card-title">Dresscode Kaos + Sablon</h5>
-                        <span>Warna</span>
-                        <div id="selectWarna" class="d-flex gap-4 mt-3">
-                            <button class="btnWarna active">Warna1</button>
-                            <button class="btnWarna">Warna2</button>
-                            <button class="btnWarna">Warna3</button>
-                            <button class="btnWarna">Warna4</button>
-                            <button class="btnWarna">Warna5</button>
-                            <button class="btnWarna">Warna6</button>
+                        <h5 class="card-title" style="font-size: 24px; font-weight: 600;">Dresscode Kaos + Sablon</h5>
+                        <span style="font-size: 20px;">Warna</span>
+                        <div id="selectWarna" class="mt-3">
+                            <div class="row">
+                                <div class="col">
+                                    <button class="btnWarna active">Warna1</button>
+                                </div>
+                                <div class="col">
+                                    <button class="btnWarna">Warna2</button>
+                                </div>
+                                <div class="col">
+                                    <button class="btnWarna">Warna3</button>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col">
+                                    <button class="btnWarna">Warna4</button>
+                                </div>
+                                <div class="col">
+                                    <button class="btnWarna">Warna5</button>
+                                </div>
+                                <div class="col">
+                                    <button class="btnWarna">Warna6</button>
+                                </div>
+                            </div>
                         </div>
                         <hr>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="d-flex flex-row align-items-center justify-content-center">
-            <button style="font-size: 18px; margin-top: 5rem; margin-bottom: 3rem; width: 800px; height: 45px;" class="d-flex align-items-center justify-content-center btn btnchat" data-bs-toggle="modal" data-bs-target="#exampleModal">Pesan Sekarang</button>
-        </div>
-        <hr>
-        <div class="container-fluid text-center">
-            <h2>PAKET EVENT TERKAIT LAINNYA</h2>
-            <br>
-            <div class="row mt-3 row-cols-md-5 g-4 row" style="text-align: start;">
-                <?php foreach ($paket as $paket) :  ?>
-                    <div class="col mb-auto">
-                        <a style="text-decoration: none;" href="<?= base_url(); ?>detail/<?= $paket['id']; ?>">
-                            <div class="card my-auto ">
-                                <img src="<?= base_url(); ?>/img/<?= $paket['gambar']; ?>" class="card-img-top img-fluid" style="">
-                                <div class="card-body">
-                                    <div style="height: 8rem">
-                                        <span style="font-size: 16px; font-weight: 600;width:100% " class="card-title"><?= $paket['nama_paket']; ?></span>
-                                    </div>
-                                    <p class="card-text  mb-1" style="color: #FFA931;font-size: 15px; font-weight: 500;">Rp <?= $paket['idr']; ?></p>
+                        <span style="font-size: 20px;">Ukuran</span>
+                        <select class="form-select mt-2 mb-4 text-center btnUkuran align-items-center" aria-label="Default select example" style="width: 209px; height: 42px; background-color: #D9D9D9; font-weight: 600; font-size: 18px;">
+                            <option value="s" selected>S</option>
+                            <option value="m">M</option>
+                            <option value="l">L</option>
+                            <option value="xl">XL</option>
+                            <option value="xxl">XXL</option>
+                        </select>
+                        <div class="jumlahForm align-items-center d-flex flex-rows"><br>
+                            <div class="d-flex flex-column">
+                                <span style="font-size: 20px;">Jumlah</span>
+                                <span style="font-size: 14px;">*minimum pemesanan 50 pcs</span>
+                            </div>
+                            <div class="input-group inline-group ms-auto">
+                                <div class="input-group-prepend">
+                                    <button class="btn btn-outline-secondary btn-minus">
+                                        <i class="fa fa-minus"></i>
+                                    </button>
+                                </div>
+                                <input class="form-control quantity" min="50" name="quantity" value="50" type="number" style="text-align: center;">
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-secondary btn-plus">
+                                        <i class="fa fa-plus"></i>
+                                    </button>
                                 </div>
                             </div>
-                        </a>
-                    </div>
-                <?php endforeach; ?>
-            </div>
-            <br><br>
-        </div>
-
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
-                <div class="modal-content">
-                    <div class="modal-body d-flex flex-column align-items-center justify-content-center">
-                        <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
-                        <h3 class="py-5" style="font-weight: 600;">Pesan Sekarang</h3>
-                        <div class="row row-cols-auto mx-5" style="height: 140px; width: 589px; align-items: center; border-color: black; border-style: solid; border-radius: 20px; border-width: 1px;">
-                            <div class="col-3">
-                                <img src="<?= base_url(); ?>/img/<?= $paketDetail['gambar']; ?>" class="img-fluid" style="height: 120px; width: auto;">
-                            </div>
-                            <div class="col-9 d-flex flex-column justify-content-center">
-                                <span><?= $paketDetail['nama_paket']; ?></span>
-                                <span style="color: #FFA931;"><?= $paketDetail['idr']; ?></span>
-                            </div>
                         </div>
-                        <div class="row row-cols-auto mx-auto pt-5" style="height: auto; width: 589px;">
-                            <span>Sebelum melanjutkan ke proses pembayaran, mohon lengkapi tanggal layanan dan metodee pembayaran terlebih dahulu</span>
-                            <form class="pt-4" style="width: 100%;">
-                                <label for="tgl" class="form-label" style="font-weight: 600;">Tanggal Layanan</label>
-                                <input type="date" class="form-control custom-form" style="border-style: none;">
-                                <label for="metodebyr" class="form-label pt-4" style="font-weight: 600;">Metode Pembayaran</label>
-                                <select class="form-select custom-form" aria-label="Default select example" style="border: none;">
-                                    <option selected></option>
-                                    <option value="1">Transfer</option>
-                                    <option value="2">Dana</option>
-                                    <option value="3">LinkAja</option>
-                                </select>
-                                <div class="d-flex flex-column text-center align-items-center py-5">
-                                    <button class="btn btn-primary">Open second modal</button>
-                                    <a type="button" class="btn" style="background-color: #FFA931; width: 285px; height: 60px;" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Bayar DP - Rp 6.000.000</a>
-                                    <br>
-                                    <button type="button" class="btn" style="background-color: #FFA931; width: 285px; height: 60px;" data-bs-toggle="modal" data-bs-target="#modalQR">Lunas - Rp 20.000.000</button>
-                                </div>
-
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
-                <div class="modal-content">
-                    <div class="modal-body d-flex flex-column align-items-center justify-content-center">
-                        <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
-                        <img src="https://www.hellotech.com/guide/wp-content/uploads/2020/05/HelloTech-qr-code-1024x1024.jpg" alt="" class="img-fluid">
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="d-flex flex-row align-items-center justify-content-center">
+        <button style="font-size: 18px; margin-top: 1rem; margin-bottom: 3rem; width: 800px; height: 45px;" class="d-flex align-items-center justify-content-center btn btnchat" data-bs-toggle="modal" data-bs-target="#exampleModal">Pesan Sekarang</button>
+    </div>
+    <hr>
+    <div class="container-fluid text-center">
+        <h2>PAKET EVENT TERKAIT LAINNYA</h2>
+        <br>
+        <div class="row mt-3 row-cols-md-5 g-4 row" style="text-align: start;">
+            <?php foreach ($paket as $paket) :  ?>
+                <div class="col mb-auto">
+                    <a style="text-decoration: none;" href="<?= base_url(); ?>detail/<?= $paket['id']; ?>">
+                        <div class="card my-auto ">
+                            <img src="<?= base_url(); ?>/img/<?= $paket['gambar']; ?>" class="card-img-top img-fluid" style="">
+                            <div class="card-body">
+                                <div style="height: 8rem">
+                                    <span style="font-size: 16px; font-weight: 600;width:100% " class="card-title"><?= $paket['nama_paket']; ?></span>
+                                </div>
+                                <p class="card-text  mb-1" style="color: #FFA931;font-size: 15px; font-weight: 500;">Rp <?= $paket['idr']; ?></p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <br><br>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-body d-flex flex-column align-items-center justify-content-center">
+                    <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <h3 class="py-5" style="font-weight: 600;">Pesan Sekarang</h3>
+                    <div class="row row-cols-auto mx-5" style="height: 140px; width: 589px; align-items: center; border-color: black; border-style: solid; border-radius: 20px; border-width: 1px;">
+                        <div class="col-3">
+                            <img src="<?= base_url(); ?>/img/<?= $paketDetail['gambar']; ?>" class="img-fluid" style="height: 120px; width: auto;">
+                        </div>
+                        <div class="col-9 d-flex flex-column justify-content-center">
+                            <span><?= $paketDetail['nama_paket']; ?></span>
+                            <span style="color: #FFA931;"><?= $paketDetail['idr']; ?></span>
+                        </div>
+                    </div>
+                    <div class="row row-cols-auto mx-auto pt-5" style="height: auto; width: 589px;">
+                        <span>Sebelum melanjutkan ke proses pembayaran, mohon lengkapi tanggal layanan dan metodee pembayaran terlebih dahulu</span>
+                        <form class="pt-4" style="width: 100%;">
+                            <label for="tgl" class="form-label" style="font-weight: 600;">Tanggal Layanan</label>
+                            <input type="date" class="form-control custom-form" style="border-style: none;">
+                            <label for="metodebyr" class="form-label pt-4" style="font-weight: 600;">Metode Pembayaran</label>
+                            <select class="form-select custom-form" aria-label="Default select example" style="border: none;">
+                                <option selected></option>
+                                <option value="1">Transfer</option>
+                                <option value="2">Dana</option>
+                                <option value="3">LinkAja</option>
+                            </select>
+                            <div class="d-flex flex-column text-center align-items-center py-5">
+                                <button class="btn btn-primary">Open second modal</button>
+                                <a type="button" class="btn" style="background-color: #FFA931; width: 285px; height: 60px;" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal">Bayar DP - Rp 6.000.000</a>
+                                <br>
+                                <button type="button" class="btn" style="background-color: #FFA931; width: 285px; height: 60px;" data-bs-toggle="modal" data-bs-target="#modalQR">Lunas - Rp 20.000.000</button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-body d-flex flex-column align-items-center justify-content-center">
+                    <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <img src="https://www.hellotech.com/guide/wp-content/uploads/2020/05/HelloTech-qr-code-1024x1024.jpg" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
     <script>
+        $('.btn-plus, .btn-minus').on('click', function(e) {
+            const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
+            const input = $(e.target).closest('.input-group').find('input');
+            if (input.is('input')) {
+                input[0][isNegative ? 'stepDown' : 'stepUp']()
+            }
+        })
+
         // Add active class to the current button (highlight it)
         var header = document.getElementById("selectWarna");
         var btns = header.getElementsByClassName("btnWarna");
