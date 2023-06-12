d38fefb27177e2bd913e82509eb816a43684af55
rasyidrazeka <rasyidrazeka@gmail.com>
2023-06-12 14:56:15 +0700

    baru

--- app/Views/detail.php
+++ app/Views/detail.php
@@ -575,76 +575,110 @@
     <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalLabel2" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered modal-lg">
             <div class="modal-content">
-                <div class="modal-body d-flex flex-column align-items-center">
+                <div class="modal-body">
                     <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
                     <div class="container-fluid">
                         <h3 class="py-5 text-center" style="font-weight: 600;">Rangkuman Pembayaran</h3>
-                        <div class="row mx-5 mt-5">
-                            <div class="col-9 d-flex justify-content-start" style="font-weight: 600;">
+                        <div class="row mx-5 mt-3">
+                            <div class="col-8 d-flex justify-content-start" style="font-weight: 600;">
                                 <h4>Detail Pemesanan</h4>
                             </div>
-                            <div class="col-3 d-flex justify-content-center" style="font-weight: 600;">
+                            <div class="col-4 d-flex justify-content-start" style="font-weight: 600;">
                                 <h4>Jumlah</h4>
                             </div>
                         </div>
                         <div class="row mx-5">
-                            <div class="col-9 d-flex justify-content-start">
+                            <div class="col-8 d-flex justify-content-start">
                                 <span>Paket Platinum Acara Dies Natalis ALEA (Decoration) 2023 untuk Mahasiswa</span>
                             </div>
-                            <div class="col-3 d-flex justify-content-center">
+                            <div class="col-4 d-flex justify-content-start">
                                 <span>Rp 20.0000.000,-</span>
                             </div>
                         </div>
                         <div class="row mx-5">
-                            <div class="col-9 justify-content-start">
+                            <div class="col-8 justify-content-start">
                                 <span>Dresscode Kaos + Sablon (All Size) Rp 60.000 x 150 pcs</span>
                             </div>
-                            <div class="col-3 justify-content-center">
-                                <span></span>
+                            <div class="col-4 justify-content-start">
+                                <span>Rp 9.0000.000,-</span>
                             </div>
+                        </div>
+                        <hr class="mx-5">
+                        <div class="row mx-5">
+                            <div class="col-8 justify-content-start" style="font-weight: 600;">
+                                <h4>Metode Pembayaran</h4>
+                            </div>
+                        </div>
+                        <div class="row mx-5">
+                            <div class="col-8 justify-content-start">
+                                <span>TRANSFER - BANK BRI</span>
+                            </div>
+                        </div>
+                        <hr class="mx-5">
+                        <div class="row mx-5">
+                            <div class="col-8 justify-content-start">
+                                <h4>Subtotal</h4>
+                            </div>
+                            <div class="col-4 justify-content-start">
+                                <span>Rp 29.0000.000,-</span>
+                            </div>
+                        </div>
+                        <hr class="mx-5">
+                        <div class="row mx-5">
+                            <div class="col-8 justify-content-start">
+                                <h4>Total Pembayaran</h4>
+                            </div>
+                            <div class="col-4 justify-content-start" style="font-weight: 600;">
+                                <span>Rp 29.0000.000,-</span>
+                            </div>
+                        </div>
+                        <hr class="mx-5">
+                        <div class="d-flex flex-column text-center align-items-center py-5">
+                            <button type="button" class="btn" style="background-color: #FFA931; width: 285px; height: 60px;" data-bs-target="#exampleModalToggle4" data-bs-toggle="modal">Lanjutkan Pembayaran</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
-        <script>
-            $('.btn-plus, .btn-minus').on('click', function(e) {
-                const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
-                const input = $(e.target).closest('.input-group').find('input');
-                if (input.is('input')) {
-                    input[0][isNegative ? 'stepDown' : 'stepUp']()
-                }
-            })
-
-            // Add active class to the current button (highlight it)
-            var header = document.getElementById("selectWarna");
-            var btns = header.getElementsByClassName("btnWarna");
-            for (var i = 0; i < btns.length; i++) {
-                btns[i].addEventListener("click", function() {
-                    var current = document.getElementsByClassName("active");
-                    current[0].className = current[0].className.replace(" active", "");
-                    this.className += " active";
-                });
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    <script>
+        $('.btn-plus, .btn-minus').on('click', function(e) {
+            const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
+            const input = $(e.target).closest('.input-group').find('input');
+            if (input.is('input')) {
+                input[0][isNegative ? 'stepDown' : 'stepUp']()
             }
+        })
 
-            function selengkapnya() {
-                var dots = document.getElementById("dots");
-                var moreText = document.getElementById("more");
-                var btnText = document.getElementById("selengkapnya");
+        // Add active class to the current button (highlight it)
+        var header = document.getElementById("selectWarna");
+        var btns = header.getElementsByClassName("btnWarna");
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function() {
+                var current = document.getElementsByClassName("active");
+                current[0].className = current[0].className.replace(" active", "");
+                this.className += " active";
+            });
+        }
 
-                if (dots.style.display === "none") {
-                    dots.style.display = "inline";
-                    btnText.innerHTML = "Lihat Lebih Banyak";
-                    moreText.style.display = "none";
-                } else {
-                    dots.style.display = "none";
-                    btnText.innerHTML = "Lihat Lebih Sedikit";
-                    moreText.style.display = "inline";
-                }
+        function selengkapnya() {
+            var dots = document.getElementById("dots");
+            var moreText = document.getElementById("more");
+            var btnText = document.getElementById("selengkapnya");
+
+            if (dots.style.display === "none") {
+                dots.style.display = "inline";
+                btnText.innerHTML = "Lihat Lebih Banyak";
+                moreText.style.display = "none";
+            } else {
+                dots.style.display = "none";
+                btnText.innerHTML = "Lihat Lebih Sedikit";
+                moreText.style.display = "inline";
             }
-        </script>
+        }
+    </script>
 </body>
 
 </html>
