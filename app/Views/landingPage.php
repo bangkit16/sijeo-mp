31ea88fe98a8f62ce53dcd2373bc5907d236318e
rasyidrazeka <rasyidrazeka@gmail.com>
2023-06-15 16:52:04 +0700

    baru

--- app/Views/landingPage.php
+++ app/Views/landingPage.php
@@ -406,8 +406,8 @@
             <div class="col-4">
 
                 <div class="card mb-3 justify-content-center mt-5" style="max-width: 100%">
-                    <div style="width: 50%;height: 5rem ;border-radius: 25px; overflow: hidden" class="m-auto mt-5">
-                        <img src="<?= base_url(); ?>/img/vector rasyid.png" class="img-fluid mx-auto" alt="..." style=";"><br>
+                    <div style="width: 50%;height: 100% ;border-radius: 100%; overflow: hidden" class="m-auto mt-5">
+                        <img src="<?= base_url(); ?>/img/review1.png" class="img-fluid mx-auto" alt="..." style=";"><br>
                     </div>
                     <div class="card-body justify-content-center m-auto">
                         <span style="color: #FFA931;" class="fa fa-star checked"></span>
@@ -417,15 +417,15 @@
                         <span class="fa fa-star"></span>
                     </div>
                     <div class="card-body justify-content-center text-center">
-                        <h5 class="card-title m-auto">Dark card title</h5>
-                        <p class="card-text m-auto">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title m-auto">Kelly</h5>
+                        <p class="card-text m-auto">SI JEO merupakan website yang sangat membantu saya dalam merencanakan pernikahan saya</p>
                     </div>
                 </div>
             </div>
             <div class="col-4">
                 <div class="card mb-3 justify-content-center mt-5" style="max-width: 100%">
-                    <div style="width: 50%;height: 5rem ;border-radius: 25px; overflow: hidden" class="m-auto mt-5">
-                        <img src="<?= base_url(); ?>/img/vector rasyid.png" class="img-fluid mx-auto" alt="..." style=";"><br>
+                    <div style="width: 50%;height: 100% ;border-radius: 100%; overflow: hidden" class="m-auto mt-5">
+                        <img src="<?= base_url(); ?>/img/review2.png" class="img-fluid mx-auto" alt="..." style=";"><br>
                     </div>
                     <div class="card-body justify-content-center m-auto">
                         <span style="color: #FFA931;" class="fa fa-star checked"></span>
@@ -435,16 +435,16 @@
                         <span class="fa fa-star"></span>
                     </div>
                     <div class="card-body justify-content-center text-center">
-                        <h5 class="card-title m-auto">Dark card title</h5>
-                        <p class="card-text m-auto">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title m-auto">Jackson</h5>
+                        <p class="card-text m-auto">Website yang sangat bermanfaat, maju terus dan terimakasih SI JEO berkat kalian pernikahan saya lancar</p>
                     </div>
                 </div>
 
             </div>
             <div class="col-4">
                 <div class="card mb-3 justify-content-center mt-5" style="max-width: 100%">
-                    <div style="width: 50%;height: 5rem ;border-radius: 25px; overflow: hidden" class="m-auto mt-5">
-                        <img src="<?= base_url(); ?>/img/vector rasyid.png" class="img-fluid mx-auto" alt="..." style=";"><br>
+                    <div style="width: 50%;height: 100% ;border-radius: 100%; overflow: hidden" class="m-auto mt-5">
+                        <img src="<?= base_url(); ?>/img/review3.png" class="img-fluid mx-auto" alt="..." style=";"><br>
                     </div>
                     <div class="card-body justify-content-center m-auto">
                         <span style="color: #FFA931;" class="fa fa-star checked"></span>
@@ -454,8 +454,8 @@
                         <span class="fa fa-star"></span>
                     </div>
                     <div class="card-body justify-content-center text-center">
-                        <h5 class="card-title m-auto">Dark card title</h5>
-                        <p class="card-text m-auto">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title m-auto">Hasan</h5>
+                        <p class="card-text m-auto">tampilan website gampang untuk dimengerti dan sistem pemesanannya pun cukup simple dan gampang</p>
                     </div>
                 </div>
             </div>
@@ -538,7 +538,7 @@
             </div>
             <div class="d-flex flex-column justify-content-center align-items-center mx-auto my-5">
                 <div>
-                    <img src="<?= base_url(); ?>/img/vector rasyid.png" class="img-fluid mb-3" style="height: 177px; width: 177px; border-radius: 100%; border: 3px solid black;">
+                    <img src="<?= base_url(); ?>/img/nadia.png" class="img-fluid mb-3" style="height: 177px; width: 177px; border-radius: 100%; border: 3px solid black;">
                 </div>
                 <div style="font-size: 24px; font-weight: 500;">Sabila Nadia I</div>
             </div>
